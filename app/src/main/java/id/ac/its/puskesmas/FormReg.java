53de9eebd994e1e3797902868fc7157ad0d73b00
Dzaky <dzakybd@gmail.com>
2016-11-24 22:06:03 +0700

    !!

--- app/src/main/java/id/ac/its/puskesmas/FormReg.java
+++ app/src/main/java/id/ac/its/puskesmas/FormReg.java
@@ -1,18 +1,41 @@
 package id.ac.its.puskesmas;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FormReg extends AppCompatActivity {
-
+    private EditText input_nama,input_alamat,input_pekerjaan,input_ttl,input_umur;
+    private Button button_daftar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registrasi);
         registrasi();
+        input_nama = (EditText) findViewById(R.id.input_nama);
+        input_alamat = (EditText) findViewById(R.id.input_alamat);
+        input_pekerjaan = (EditText) findViewById(R.id.input_pekerjaan);
+        input_ttl = (EditText) findViewById(R.id.input_ttl);
+        input_umur = (EditText) findViewById(R.id.input_umur);
+        input_umur = (EditText) findViewById(R.id.input_umur);
+        button_daftar = (Button) findViewById(R.id.button_daftar);
+        button_daftar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Pasien pasien = new Pasien();
+                pasien.setNama(input_nama.getText().toString());
+                pasien.setAlamat(input_alamat.getText().toString());
+                pasien.setLahir(input_ttl.getText().toString());
+                pasien.setPekerjaan(input_ttl.getText().toString());
+                pasien.setUmur(Integer.parseInt(input_umur.getText().toString()));
+                Snackbar.make(v, "Terdaftar", Snackbar.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 
     private void registrasi(){
-
     }
 }
