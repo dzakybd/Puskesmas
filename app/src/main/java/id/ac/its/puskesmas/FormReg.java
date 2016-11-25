c81c34720fb8b1d9d59938dc9621f7616d6b4113
Dzaky <dzakybd@gmail.com>
2016-11-25 20:10:11 +0700

    !!

--- app/src/main/java/id/ac/its/puskesmas/FormReg.java
+++ app/src/main/java/id/ac/its/puskesmas/FormReg.java
@@ -1,41 +1,48 @@
 package id.ac.its.puskesmas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class FormReg extends AppCompatActivity {
     private EditText input_nama,input_alamat,input_pekerjaan,input_ttl,input_umur;
-    private Button button_daftar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registrasi);
-        registrasi();
         input_nama = (EditText) findViewById(R.id.input_nama);
         input_alamat = (EditText) findViewById(R.id.input_alamat);
         input_pekerjaan = (EditText) findViewById(R.id.input_pekerjaan);
         input_ttl = (EditText) findViewById(R.id.input_ttl);
         input_umur = (EditText) findViewById(R.id.input_umur);
         input_umur = (EditText) findViewById(R.id.input_umur);
-        button_daftar = (Button) findViewById(R.id.button_daftar);
-        button_daftar.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Pasien pasien = new Pasien();
-                pasien.setNama(input_nama.getText().toString());
-                pasien.setAlamat(input_alamat.getText().toString());
-                pasien.setLahir(input_ttl.getText().toString());
-                pasien.setPekerjaan(input_ttl.getText().toString());
-                pasien.setUmur(Integer.parseInt(input_umur.getText().toString()));
-                Snackbar.make(v, "Terdaftar", Snackbar.LENGTH_LONG)
-                        .show();
-            }
-        });
     }
 
-    private void registrasi(){
+    public void registrasi(View v){
+        setdata();
+        Snackbar.make(v, "Terdaftar", Snackbar.LENGTH_LONG)
+                .show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(FormReg.this, FormKeluhan.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
     }
+
+    public void setdata(){
+        Pasien pasien = new Pasien();
+        pasien.setNama(input_nama.getText().toString());
+        pasien.setAlamat(input_alamat.getText().toString());
+        pasien.setLahir(input_ttl.getText().toString());
+        pasien.setPekerjaan(input_ttl.getText().toString());
+        pasien.setUmur(Integer.parseInt(input_umur.getText().toString()));
+    }
+
 }
